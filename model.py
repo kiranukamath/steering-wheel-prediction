7feb846920d8dd5c06fc0ba61684b8eb1f082e26
MEGHANA G S <meghanamoni12@gmail.com>
2020-04-19 13:32:18 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+
+class NetworkDense(nn.Module):
+
+    def __init__(self):
+        super(NetworkDense, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, 24, 5, stride=2),
+            nn.ELU(),
+            nn.Conv2d(24, 36, 5, stride=2),
+            nn.ELU(),
+            nn.Conv2d(36, 48, 5, stride=2),
+            nn.ELU(),
+            nn.Conv2d(48, 64, 3),
+            nn.ELU(),
+            nn.Conv2d(64, 64, 3),
+            nn.Dropout(0.25)
+        )
+        self.linear_layers = nn.Sequential(
+            nn.Linear(in_features=64 * 2 * 33, out_features=100),
+            nn.ELU(),
+            nn.Linear(in_features=100, out_features=50),
+            nn.ELU(),
+            nn.Linear(in_features=50, out_features=10),
+            nn.Linear(in_features=10, out_features=1)
+        )
+        
+    def forward(self, input):  
+        input = input.view(input.size(0), 3, 70, 320)
+        output = self.conv_layers(input)
+        output = output.view(output.size(0), -1)
+        output = self.linear_layers(output)
+        return output
